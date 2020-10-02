849435e7d10539be06c2da39c787a84cbb846588
Wilian Gabriel <wilian.silva@zup.com.br>
2020-10-02 16:13:07 -0300

    Fixing tests

--- horusec-cli/internal/services/formatters/service_test.go
+++ horusec-cli/internal/services/formatters/service_test.go
@@ -77,7 +77,7 @@ func TestGetConfigProjectPath(t *testing.T) {
 		result := monitorController.GetConfigProjectPath()
 
 		assert.NotEmpty(t, result)
-		assert.Equal(t, "test", result)
+		assert.Equal(t, "test/.horusec/00000000-0000-0000-0000-000000000000", result)
 	})
 }
 
