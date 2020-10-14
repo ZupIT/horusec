03fd80e3f45319337774ba12024b446b0dd63afe
nathan <nathan.martins@zup.com.br>
2020-10-14 09:14:45 -0300

    Adding horusec auth base project

---
+++ horusec-auth/cmd/app/main.go
@@ -0,0 +1,31 @@
+// Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//nolint
+package main
+
+// @title Horusec-Auth
+// @description Service of Horusec.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Horusec
+// @contact.url https://github.com/ZupIT/horusec
+// @contact.email horusec@zup.com.br
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+func main() {
+
+}
---
+++ horusec-auth/internal/services/auth_mock.go
@@ -0,0 +1,34 @@
+// Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package services
+
+import (
+	auth "github.com/ZupIT/horusec/development-kit/pkg/entities/credentials"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockAuthService struct {
+	mock.Mock
+}
+
+func (m *MockAuthService) Authenticate(credentials auth.Credentials) (bool, map[string]string, error) {
+	args := m.MethodCalled("Authenticate", credentials)
+	return args.Bool(0), args.Get(1).(map[string]string), args.Error(2)
+}
+
+func (m *MockAuthService) IsAuthorized(userID string, group []string) (bool, error) {
+	args := m.MethodCalled("IsAuthorized", userID, group)
+	return args.Bool(0), args.Error(1)
+}
