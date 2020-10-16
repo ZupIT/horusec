c44b702e0f15225c79405dae7576d9c9b95f4935
Lucas Bruno <69604366+lucasbrunozup@users.noreply.github.com>
2020-10-16 17:28:42 -0300

    [Frontend] - Improvements false positive (#69)
    * Added new filter in false positive screen
    
    * Added success messages
    
    * Added tag with color in severity
    
    * Removing supervisor role in company
    
    * Added permission to handler repository

--- horusec-manager/src/pages/Internal/Home/Vulnerabilities/index.tsx
+++ horusec-manager/src/pages/Internal/Home/Vulnerabilities/index.tsx
@@ -30,7 +30,7 @@ import Details from './Details';
 import { FilterVuln } from 'helpers/interfaces/FIlterVuln';
 import useFlashMessage from 'helpers/hooks/useFlashMessage';
 import { useTheme } from 'styled-components';
-import { get } from 'lodash';
+import { get, find } from 'lodash';
 
 const INITIAL_PAGE = 1;
 
@@ -139,10 +139,18 @@ const Vulnerabilities: React.FC = () => {
       })
       .catch((err) => {
         dispatchMessage(err?.response?.data);
+        setVulnerabilities([]);
       })
       .finally(() => {
         setLoading(false);
       });
+  };
+
+  const isAdminOrSupervisorOfRepository = () => {
+    const repository = find(repositories, {
+      repositoryID: filters.repositoryID,
+    });
+    return repository.role === 'admin' || repository.role === 'supervisor';
   };
 
   const handleSearch = debounce((searchString: string) => {
@@ -179,6 +187,8 @@ const Vulnerabilities: React.FC = () => {
             { ...filters, repositoryID: result.data?.content[0].repositoryID },
             pagination
           );
+        } else {
+          setLoading(false);
         }
       });
     };
@@ -314,6 +324,7 @@ const Vulnerabilities: React.FC = () => {
                       rounded
                       initialValue={vul.type}
                       options={vulnTypes}
+                      disabled={!isAdminOrSupervisorOfRepository()}
                       onChangeValue={(value) =>
                         handleUpdateVulnerabilityType(vul, value.value)
                       }
