f8fa241373476d1cfc2b9a459daac39ff0af0eef
Horusec <horusec@zup.com.br>
2020-10-19 16:55:09 -0300

    Updating ldap to implement auth service

--- horusec-auth/internal/services/ldap/ldap.go
+++ horusec-auth/internal/services/ldap/ldap.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	auth "github.com/ZupIT/horusec/development-kit/pkg/entities/auth"
+	"github.com/ZupIT/horusec/development-kit/pkg/services/jwt"
 	ldapconfig "github.com/ZupIT/horusec/horusec-auth/config/ldap"
 	"github.com/ZupIT/horusec/horusec-auth/internal/services"
 	ldapclient "github.com/jtblin/go-ldap-client"
@@ -13,25 +14,39 @@ type Service struct {
 	client ldapclient.LDAPClient
 }
 
-func NewService() services.AuthService {
+func NewService() services.IAuthService {
 	return &Service{
 		client: ldapconfig.NewLDAPClient(),
 	}
 }
 
-func (s *Service) Authenticate(credentials *auth.Credentials) (bool, map[string]string, error) {
-	return s.client.Authenticate(credentials.Username, credentials.Password)
+func (s *Service) Authenticate(credentials *auth.Credentials) (interface{}, error) {
+	var result map[string]interface{}
+
+	ok, data, err := s.client.Authenticate(credentials.Username, credentials.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	result["ok"] = ok
+	result["data"] = data
+
+	return result, nil
 }
 
-func (s *Service) IsAuthorized(userID string, groups []string) (bool, error) {
-	userGroups, err := s.client.GetGroupsOfUser(userID)
+func (s *Service) IsAuthorized(authzData *auth.AuthorizationData) (bool, error) {
+	token, err := jwt.DecodeToken(authzData.Token)
+	if err != nil {
+		return false, err
+	}
 
+	userGroups, err := s.client.GetGroupsOfUser(token.Username)
 	if err != nil {
 		return false, err
 	}
 
 	for _, userGroup := range userGroups {
-		if s.contains(groups, userGroup) {
+		if s.contains(authzData.Groups, userGroup) {
 			return true, nil
 		}
 	}
