8f4f92fd6537d303af0d11f36315f3fbca123acd
Horusec <horusec@zup.com.br>
2020-10-21 15:01:17 -0300

    WIP ldap authz

--- horusec-auth/internal/services/ldap/ldap.go
+++ horusec-auth/internal/services/ldap/ldap.go
@@ -3,20 +3,29 @@ package ldap
 import (
 	"errors"
 
+	"github.com/ZupIT/horusec/development-kit/pkg/databases/relational"
+	"github.com/ZupIT/horusec/development-kit/pkg/databases/relational/repository/account"
+	"github.com/ZupIT/horusec/development-kit/pkg/databases/relational/repository/company"
 	auth "github.com/ZupIT/horusec/development-kit/pkg/entities/auth"
 	"github.com/ZupIT/horusec/development-kit/pkg/services/jwt"
 	ldapconfig "github.com/ZupIT/horusec/horusec-auth/config/ldap"
 	"github.com/ZupIT/horusec/horusec-auth/internal/services"
+	"github.com/google/uuid"
 	ldapclient "github.com/jtblin/go-ldap-client"
 )
 
 type Service struct {
-	client ldapclient.LDAPClient
+	client      ldapclient.LDAPClient
+	accountRepo account.IAccount
+	companyRepo company.ICompanyRepository
 }
 
-func NewService() services.IAuthService {
+func NewService(
+	databaseRead relational.InterfaceRead, databaseWrite relational.InterfaceWrite) services.IAuthService {
 	return &Service{
-		client: ldapconfig.NewLDAPClient(),
+		client:      ldapconfig.NewLDAPClient(),
+		accountRepo: account.NewAccountRepository(databaseRead, databaseWrite),
+		companyRepo: company.NewCompanyRepository(databaseRead, databaseWrite),
 	}
 }
 
@@ -35,12 +44,7 @@ func (s *Service) Authenticate(credentials *auth.Credentials) (interface{}, erro
 }
 
 func (s *Service) IsAuthorized(authzData *auth.AuthorizationData) (bool, error) {
-	token, err := jwt.DecodeToken(authzData.Token)
-	if err != nil {
-		return false, err
-	}
-
-	userGroups, err := s.client.GetGroupsOfUser(token.Username)
+	userGroups, err := s.getUserGroups(authzData.Token)
 	if err != nil {
 		return false, err
 	}
@@ -52,6 +56,27 @@ func (s *Service) IsAuthorized(authzData *auth.AuthorizationData) (bool, error) 
 	}
 
 	return false, errors.New("not authorized")
+}
+
+func (s *Service) getCompanyAuthzGroupsName(companyID uuid.UUID) ([]string, error) {
+	company, err := s.companyRepo.GetByID(companyID)
+	if err != nil {
+		return nil, err
+	}
+}
+
+func (s *Service) getUserGroups(tokenStr string) ([]string, error) {
+	token, err := jwt.DecodeToken(tokenStr)
+	if err != nil {
+		return nil, err
+	}
+
+	userGroups, err := s.client.GetGroupsOfUser(token.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	return userGroups, nil
 }
 
 func (s *Service) contains(collection []string, value string) bool {
