ad138f25b9860b87be6027e050b8377ad1a88715
Horusec <horusec@zup.com.br>
2020-10-23 13:53:57 -0300

    Improving ldap client service

--- development-kit/pkg/services/ldap/ldap.go
+++ development-kit/pkg/services/ldap/ldap.go
@@ -24,33 +24,31 @@ type ILDAPService interface {
 	GetGroupsOfUser(username string) ([]string, error)
 }
 
-type LDAPService struct {
+type Service struct {
 	client goldapclient.LDAPClient
 }
 
 func NewLDAPClient() ILDAPService {
-	client := goldapclient.LDAPClient{
-		Base:               env.GetEnvOrDefault("HORUS_LDAP_BASE", ""),
-		Host:               env.GetEnvOrDefault("HORUS_LDAP_HOST", ""),
-		Port:               env.GetEnvOrDefaultInt("HORUS_LDAP_PORT", 0),
-		UseSSL:             env.GetEnvOrDefaultBool("HORUS_LDAP_USESSL", false),
-		SkipTLS:            env.GetEnvOrDefaultBool("HORUS_LDAP_SKIP_TLS", true),
-		InsecureSkipVerify: env.GetEnvOrDefaultBool("HORUS_LDAP_INSECURE_SKIP_VERIFY", true),
-		BindDN:             env.GetEnvOrDefault("HORUS_LDAP_BINDDN", ""),
-		BindPassword:       env.GetEnvOrDefault("HORUS_LDAP_BINDPASSWORD", ""),
-		UserFilter:         env.GetEnvOrDefault("HORUS_LDAP_USERFILTER", ""),
-		GroupFilter:        env.GetEnvOrDefault("HORUS_LDAP_GROUPFILTER", ""),
-	}
-
-	return &LDAPService{
-		client: client,
+	return &Service{
+		client: goldapclient.LDAPClient{
+			Base:               env.GetEnvOrDefault("HORUS_LDAP_BASE", ""),
+			Host:               env.GetEnvOrDefault("HORUS_LDAP_HOST", ""),
+			Port:               env.GetEnvOrDefaultInt("HORUS_LDAP_PORT", 0),
+			UseSSL:             env.GetEnvOrDefaultBool("HORUS_LDAP_USESSL", false),
+			SkipTLS:            env.GetEnvOrDefaultBool("HORUS_LDAP_SKIP_TLS", true),
+			InsecureSkipVerify: env.GetEnvOrDefaultBool("HORUS_LDAP_INSECURE_SKIP_VERIFY", true),
+			BindDN:             env.GetEnvOrDefault("HORUS_LDAP_BINDDN", ""),
+			BindPassword:       env.GetEnvOrDefault("HORUS_LDAP_BINDPASSWORD", ""),
+			UserFilter:         env.GetEnvOrDefault("HORUS_LDAP_USERFILTER", ""),
+			GroupFilter:        env.GetEnvOrDefault("HORUS_LDAP_GROUPFILTER", ""),
+		},
 	}
 }
 
-func (s *LDAPService) Authenticate(username, password string) (bool, map[string]string, error) {
+func (s *Service) Authenticate(username, password string) (bool, map[string]string, error) {
 	return s.client.Authenticate(username, password)
 }
 
-func (s *LDAPService) GetGroupsOfUser(username string) ([]string, error) {
+func (s *Service) GetGroupsOfUser(username string) ([]string, error) {
 	return s.client.GetGroupsOfUser(username)
 }
