09db8677869e44296573e834f3a04f2f16171397
nathan <nathan.martins@zup.com.br>
2020-11-04 16:56:44 -0300

    Updating compose and grpc config

--- horusec-api/config/grpc/grpc.go
+++ horusec-api/config/grpc/grpc.go
@@ -1,12 +1,13 @@
 package grpc
 
 import (
+	"github.com/ZupIT/horusec/development-kit/pkg/utils/env"
 	"github.com/ZupIT/horusec/development-kit/pkg/utils/logger"
 	"google.golang.org/grpc"
 )
 
 func SetupGrpcConnection() *grpc.ClientConn {
-	conn, err := grpc.Dial("localhost:8007", grpc.WithInsecure())
+	conn, err := grpc.Dial(env.GetEnvOrDefault("HORUSEC_AUTH_URL", "localhost:8007"), grpc.WithInsecure())
 	if err != nil {
 		logger.LogPanic("failed to connect to auth grpc", err)
 	}
