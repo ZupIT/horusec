ab90afeb24fa1c75282cfaa7a7d9349832b3531b
nathan <nathan.martins@zup.com.br>
2020-11-04 14:12:25 -0300

    Fixing middleware tests

---
+++ development-kit/pkg/services/grpc/auth/mock.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+	mockUtils "github.com/ZupIT/horusec/development-kit/pkg/utils/mock"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc"
+)
+
+type Mock struct {
+	mock.Mock
+}
+
+func (m *Mock) IsAuthorized(_ context.Context, _ *IsAuthorizedData, _ ...grpc.CallOption) (*IsAuthorizedResponse, error) {
+	args := m.MethodCalled("IsAuthorized")
+	return args.Get(0).(*IsAuthorizedResponse), mockUtils.ReturnNilOrError(args, 1)
+}
+
+func (m *Mock) GetAccountID(ctx context.Context, in *GetAccountIDData, opts ...grpc.CallOption) (*GetAccountIDResponse, error) {
+	args := m.MethodCalled("GetAccountID")
+	return args.Get(0).(*GetAccountIDResponse), mockUtils.ReturnNilOrError(args, 1)
+}
+
+func (m *Mock) GetAuthConfig(ctx context.Context, in *GetAuthConfigData, opts ...grpc.CallOption) (*GetAuthConfigResponse, error) {
+	args := m.MethodCalled("GetAuthConfig")
+	return args.Get(0).(*GetAuthConfigResponse), mockUtils.ReturnNilOrError(args, 1)
+}
