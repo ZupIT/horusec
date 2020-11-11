662d8825609e6904eec8e6ead97db2ced0332c4f
Wilian Gabriel <wilian.silva@zup.com.br>
2020-11-11 10:45:58 -0300

    Start crud of webhook

--- development-kit/pkg/entities/webhook/webhook.go
+++ development-kit/pkg/entities/webhook/webhook.go
@@ -1,17 +1,32 @@
 package webhook
 
 import (
+	"encoding/json"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/google/uuid"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type Webhook struct {
-	WebhookID    uuid.UUID         `json:"webhookID"`
+	WebhookID    uuid.UUID         `json:"webhookID" gorm:"primary_key" swaggerignore:"true"`
 	URL          string            `json:"url"`
 	Method       string            `json:"method"`
 	Headers      map[string]string `json:"headers"`
+	RepositoryID uuid.UUID         `json:"repositoryID" swaggerignore:"true"`
+	CompanyID    uuid.UUID         `json:"companyID" swaggerignore:"true"`
+}
+
+type WebhookResponse struct {
+	WebhookID   uuid.UUID      `json:"webhookID"`
+	Method       string            `json:"method"`
+	Headers      map[string]string `json:"headers"`
 	RepositoryID uuid.UUID         `json:"repositoryID"`
+	CompanyID    uuid.UUID         `json:"companyID"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
 }
 
 func (w *Webhook) GetTable() string {
@@ -29,4 +44,20 @@ func (w *Webhook) GetMethod() string {
 	default:
 		return ""
 	}
+}
+
+func (w *Webhook) Validate() error {
+	return validation.ValidateStruct(w,
+		validation.Field(&w.URL, validation.Required, is.URL),
+		validation.Field(&w.Method, validation.Required, validation.In(
+			http.MethodPost, http.MethodPut, http.MethodPatch,
+		)),
+		validation.Field(&w.RepositoryID, validation.Required, is.UUID),
+		validation.Field(&w.CompanyID, validation.Required, is.UUID),
+	)
+}
+
+func (w *Webhook) ToBytes() []byte {
+	bytes, _ := json.Marshal(w)
+	return bytes
 }
