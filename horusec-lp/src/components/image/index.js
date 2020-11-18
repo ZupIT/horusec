0601723ae9b2208d1aa115e048f7c20dc8507db2
Wilian Gabriel <wilian.silva@zup.com.br>
2020-11-18 08:34:41 -0300

    Removing Landing Page

--- horusec-lp/src/components/image/index.js
+++
@@ -1,6 +0,0 @@
-export {default as AboutImage} from './about-image';
-export {default as ManageImage} from './manage-image';
-export {default as PipelineFirstImage} from './pipeline-image-1';
-export {default as PipelineSecondImage} from './pipeline-image-2';
-export {default as LogoHorussec} from './logo-horussec';
-export {default as ExampleIllustration} from './example-illustration';
