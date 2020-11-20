2072c4a45fc14dda71f5ef5e6dd0ac844c5929d4
Wilian Gabriel <wilian.silva@zup.com.br>
2020-11-20 11:46:03 -0300

    Fixing log very sensitive in csharp

--- horusec-cli/internal/services/formatters/csharp/horuseccsharp/config.go
+++ horusec-cli/internal/services/formatters/csharp/horuseccsharp/config.go
@@ -16,7 +16,7 @@ package horuseccsharp
 
 const (
 	ImageName = "horuszup/horusec-csharp"
-	ImageTag  = "v0.0.3"
+	ImageTag  = "v0.0.4"
 	ImageCmd  = `
 		{{WORK_DIR}}
 		horusec-csharp run -o="./output-ANALYSISID.json"
