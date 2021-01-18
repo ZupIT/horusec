73a7a81e34972ceffeff9b63b946cdbef3f9c34b
Wilian Gabriel <63816070+wiliansilvazup@users.noreply.github.com>
2021-01-18 17:32:12 -0300

    Feature/improvements cli (#279)
    * Adding warn log when info is disabled and update engines to run max units by analysis
    
    * Adding const values for engine usage
    
    Co-authored-by: Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>

--- horusec-cli/internal/services/formatters/csharp/horuseccsharp/formatter.go
+++ horusec-cli/internal/services/formatters/csharp/horuseccsharp/formatter.go
@@ -17,6 +17,7 @@ package horuseccsharp
 import (
 	engine "github.com/ZupIT/horusec-engine"
 	"github.com/ZupIT/horusec/development-kit/pkg/engines/csharp"
+	engineenums "github.com/ZupIT/horusec/development-kit/pkg/enums/engine"
 	"github.com/ZupIT/horusec/development-kit/pkg/enums/languages"
 	"github.com/ZupIT/horusec/development-kit/pkg/enums/tools"
 	"github.com/ZupIT/horusec/development-kit/pkg/utils/logger"
@@ -65,5 +66,5 @@ func (f *Formatter) execEngineAnalysis(projectSubPath string) ([]engine.Finding,
 	}
 
 	allRules := append(f.GetAllRules(), f.GetCustomRulesByTool(tools.HorusecCsharp)...)
-	return engine.Run(textUnit, allRules), nil
+	return engine.RunMaxUnitsByAnalysis(textUnit, allRules, engineenums.DefaultMaxUnitsPerAnalysis), nil
 }
