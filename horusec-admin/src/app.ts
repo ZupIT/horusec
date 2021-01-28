0b617eac3769c4364dd390a77e86bc36f902c6a0
Wilian Gabriel <wilian.silva@zup.com.br>
2021-01-28 08:47:19 -0300

    Fix horusec validation

--- horusec-admin/src/app.ts
+++ horusec-admin/src/app.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 import { Config, IConfig } from "./config/config";
 import { Database } from "./database/postgresql";
 import { Express } from "express-serve-static-core";
-import { AppRoutes } from "./routes/app";
+import { AppRoutes } from "./routes/horusec";
 import { TokenUtil } from "./utils/token";
 
 const config: IConfig = new Config().getConfig();
@@ -11,14 +11,14 @@ const db: Sequelize = new Database().getConnection();
 const app: Express = express();
 const token: TokenUtil = new TokenUtil();
 const accessToken: string = token.generateToken();
-const routes: AppRoutes = new AppRoutes(db, app, accessToken);
+const routes: AppRoutes = new AppRoutes(db, accessToken);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-routes.start();
+routes.start(app);
 
 app.listen(config.Port, () => console.warn(`
   app is running on http://localhost:${config.Port}
