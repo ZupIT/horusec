2dacb21e0f27b8e7909dfe116b8796a618b2064c
Lucas Bruno <69604366+lucasbrunozup@users.noreply.github.com>
2021-02-23 09:47:26 -0300

    Added search and increase size of select of repositories (#356)
    * Added search and increase size of select of repositories
    
    * Added hash of vuln to ignore
    
    * Fixing select menu when initialValue is a object

--- horusec-manager/src/pages/Internal/Vulnerabilities/index.tsx
+++ horusec-manager/src/pages/Internal/Vulnerabilities/index.tsx
@@ -248,7 +248,8 @@ const Vulnerabilities: React.FC = () => {
         <Select
           keyLabel="name"
           width="220px"
-          optionsHeight="100px"
+          optionsHeight="200px"
+          hasSearch
           rounded
           initialValue={repositories[0]}
           options={repositories}
