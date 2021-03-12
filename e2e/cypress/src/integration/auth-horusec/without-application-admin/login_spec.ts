e5e603bfc93840bfa083a008c1a00a4cf8023d44
wilian <wilian.silva@zup.com.br>
2021-03-12 19:02:29 -0300

    Adding unit test

--- e2e/cypress/src/integration/auth-horusec/without-application-admin/login_spec.ts
+++ e2e/cypress/src/integration/auth-horusec/without-application-admin/login_spec.ts
@@ -1,9 +1,10 @@
 describe("Login", () => {
     beforeEach(() => {
-        cy.visit("https://manager.horus.dev.zup.corp/auth");
+        cy.visit("https://localhost:8043");
     });
 
     it("Should login with default account", () => {
+        cy.wait(2000)
         cy.get("#email").type("dev@example.com");
         cy.get("#password").type("Devpass0*");
         cy.get("button").first().click();
