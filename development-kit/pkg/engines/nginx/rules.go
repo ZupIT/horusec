3b9e6b5cef8ac1fc7fc4931ffe7db70a463da63a
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-04-26 17:06:44 -0300

    Adding nginx rules loader

--- development-kit/pkg/engines/nginx/rules.go
+++ development-kit/pkg/engines/nginx/rules.go
@@ -12,4 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//nolint:lll multiple regex is not possible broken lines
 package nginx
+
+import (
+	engine "github.com/ZupIT/horusec-engine"
+	"github.com/ZupIT/horusec-engine/text"
+	"github.com/ZupIT/horusec/development-kit/pkg/engines/nginx/not"
+)
+
+type Interface interface {
+	GetAllRules() (rules []engine.Rule)
+	GetTextUnitByRulesExt(projectPath string) ([]engine.Unit, error)
+}
+
+type Rules struct{}
+
+func NewRules() Interface {
+	return &Rules{}
+}
+
+func (r *Rules) GetAllRules() (rules []engine.Rule) {
+	for _, rule := range allNginxNotRules() {
+		rules = append(rules, rule)
+	}
+
+	return rules
+}
+
+func (r *Rules) GetTextUnitByRulesExt(projectPath string) ([]engine.Unit, error) {
+	textUnits, err := text.LoadDirIntoMultiUnit(projectPath, 5, r.getExtensions())
+	if err != nil {
+		return []engine.Unit{}, err
+	}
+	return r.parseTextUnitsToUnits(textUnits), nil
+}
+
+func (r *Rules) parseTextUnitsToUnits(textUnits []text.TextUnit) (units []engine.Unit) {
+	for index := range textUnits {
+		units = append(units, textUnits[index])
+	}
+	return units
+}
+
+func (r *Rules) getExtensions() []string {
+	return []string{".nginx", "nginxconf", ".vhost"}
+}
+
+func allNginxNotRules() []text.TextRule {
+	return []text.TextRule{
+		not.NewNginxNotIncludeXFrameOptionsHeader(),
+	}
+}
