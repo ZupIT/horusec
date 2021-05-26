8800dd8a783635937982050f9dc4d4cd3c49c360
matheusalcantarazup <84723211+matheusalcantarazup@users.noreply.github.com>
2021-05-26 10:02:54 -0300

    fix typo on debug message (#462)

--- internal/helpers/messages/debug.go
+++ internal/helpers/messages/debug.go
@@ -38,7 +38,7 @@ const (
 	MsgDebugConfigFileRunningOnPath  = "{HORUSEC_CLI} Config file running on path: "
 	MsgDebugConfigFileNotFoundOnPath = "{HORUSEC_CLI} Config file not found"
 	// Fired when occurs of ignore folder or file to send horusec analysis
-	MsgDebugFolderOrFileIgnored = "{HORUSEC_CLI} The file ou folder was ignored to send analysis:"
+	MsgDebugFolderOrFileIgnored = "{HORUSEC_CLI} The file or folder was ignored to send analysis:"
 	// Fired when configs already validate and before start analysis
 	MsgDebugShowConfigs   = "{HORUSEC_CLI} The current configuration for this analysis are:"
 	MsgDebugShowWorkdir   = "{HORUSEC_CLI} The workdir setup for run in path:"
