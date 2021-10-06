26c6509e6b6044df2230ea2163824593e01eb014
Ian Cardoso <ian.cardoso@zup.com.br>
2021-10-06 13:38:16 -0300

    trivy: fix trivy parsing error
    Trivy docker container was shared for both checks and both commands were writing to same file amending 2 jsons incorrectly, this commit change it to different files to solve this problem
    
    Signed-off-by: Ian Cardoso <ian.cardoso@zup.com.br>

--- internal/services/formatters/generic/trivy/config.go
+++ internal/services/formatters/generic/trivy/config.go
@@ -22,12 +22,12 @@ func (c Cmd) ToString() string {
 
 const CmdFs Cmd = `
 		{{WORK_DIR}}
-		TRIVY_NEW_JSON_SCHEMA=true trivy fs  -f json -o result.json ./ &> /dev/null
-		cat result.json
+		TRIVY_NEW_JSON_SCHEMA=true trivy fs  -f json -o resultFs.json ./ &> /dev/null
+		cat resultFs.json
   `
 
 const CmdConfig Cmd = `
 		{{WORK_DIR}}
-		TRIVY_NEW_JSON_SCHEMA=true trivy config -f json -o result.json ./ &> /dev/null 
-		cat result.json
+		TRIVY_NEW_JSON_SCHEMA=true trivy config -f json -o resultConfig.json ./ &> /dev/null 
+		cat resultConfig.json
   `
