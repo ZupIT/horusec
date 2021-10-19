cf5bc0042067505bcacd38f372574b617869e14b
lucas.bruno <lucas.bruno@zup.com.br>
2021-10-19 16:16:20 -0300

    e2e: refactor structure for e2e tests to use ginkgo as test runner and gomega as test matcher.
    Signed-off-by: lucas.bruno <lucas.bruno@zup.com.br>

---
+++ e2e/scan/scan_test.go
@@ -0,0 +1,167 @@
+// Copyright 2021 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scan_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ZupIT/horusec-devkit/pkg/entities/analysis"
+	"github.com/ZupIT/horusec/internal/utils/testutil"
+)
+
+var _ = Describe("Scan vulnerabilities example folder", func() {
+	testcases := []struct {
+		name            string
+		target          string
+		vulnerabilities int
+	}{
+		{
+			name:            "Leaks",
+			target:          testutil.LeaksExample1,
+			vulnerabilities: 25,
+		},
+		{
+			name:            "Go",
+			target:          testutil.GoExample1,
+			vulnerabilities: 19,
+		},
+		{
+			name:            "Csharp",
+			target:          testutil.CsharpExample1,
+			vulnerabilities: 13,
+		},
+		{
+			name:            "Ruby",
+			target:          testutil.RubyExample1,
+			vulnerabilities: 55,
+		},
+		{
+			name:            "PythonBandit",
+			target:          testutil.PythonExample1,
+			vulnerabilities: 6,
+		},
+		{
+			name:            "PythonSafety",
+			target:          testutil.PythonExample2,
+			vulnerabilities: 20,
+		},
+		{
+			name:            "Java",
+			target:          testutil.JavaExample1,
+			vulnerabilities: 1,
+		},
+		{
+			name:            "Kotlin",
+			target:          testutil.KotlinExample1,
+			vulnerabilities: 1,
+		},
+		{
+			name:            "JavascriptNPM",
+			target:          testutil.JavaScriptExample1,
+			vulnerabilities: 29,
+		},
+		{
+			name:            "JavascriptYarn",
+			target:          testutil.JavaScriptExample2,
+			vulnerabilities: 28,
+		},
+		{
+			name:            "HCL",
+			target:          testutil.Hclxample1,
+			vulnerabilities: 7,
+		},
+		{
+			name:            "Dart",
+			target:          testutil.DartExample1,
+			vulnerabilities: 3,
+		},
+		{
+			name:            "PHP",
+			target:          testutil.PHPExample1,
+			vulnerabilities: 12,
+		},
+		{
+			name:            "Yaml",
+			target:          testutil.YamlExample1,
+			vulnerabilities: 1,
+		},
+		{
+			name:            "Elixir",
+			target:          testutil.ElixirExample1,
+			vulnerabilities: 3,
+		},
+		{
+			name:            "Nginx",
+			target:          testutil.NginxExample1,
+			vulnerabilities: 4,
+		},
+		{
+			name:            "Swift",
+			target:          testutil.SwiftExample1,
+			vulnerabilities: 17,
+		},
+	}
+
+	for _, tt := range testcases {
+		It(tt.name, func() {
+			bin := testutil.GomegaBuildHorusecBinary()
+			output := path.Join(os.TempDir(), fmt.Sprintf("horusec-analysis-%s.json", uuid.New().String()))
+
+			flags := map[string]string{
+				"-p": tt.target,
+				"-o": "json",
+				"-O": output,
+			}
+
+			cmdArguments := []string{
+				"start",
+			}
+
+			for flag, value := range flags {
+				cmdArguments = append(cmdArguments, fmt.Sprintf("%s=%s", flag, value))
+			}
+
+			cmd := exec.Command(bin, cmdArguments...)
+			err := cmd.Run()
+
+			if err != nil {
+				Fail("Error on run CLI to scan tests")
+			}
+
+			fileContent, err := os.ReadFile(output)
+
+			if err != nil {
+				Fail("Error on read file for scan on e2e test")
+			}
+
+			var horusecAnalysis analysis.Analysis
+			err = json.Unmarshal(fileContent, &horusecAnalysis)
+
+			if err != nil {
+				Fail("Error on read json result for horusec scan example folder")
+			}
+
+			Expect(tt.vulnerabilities).Should(Equal(len(horusecAnalysis.AnalysisVulnerabilities)))
+		})
+	}
+})
