fdfb44a8c199cee050ccacf30f9065dbe7186ae1
Nathan Martins <nathan.martins@zup.com.br>
2021-10-26 16:38:51 -0300

    ignore: adding .tar, .zip and .exe to the list of ignored extensions
    Signed-off-by: Nathan Martins <nathan.martins@zup.com.br>

--- internal/enums/toignore/to_ignore_test.go
+++ internal/enums/toignore/to_ignore_test.go
@@ -27,7 +27,7 @@ func TestGetDefaultFoldersToIgnore(t *testing.T) {
 }
 
 func TestGetDefaultExtensionsToIgnore(t *testing.T) {
-	t.Run("should success get 35 extensions to ignore", func(t *testing.T) {
-		assert.Len(t, GetDefaultExtensionsToIgnore(), 35)
+	t.Run("should success get 38 extensions to ignore", func(t *testing.T) {
+		assert.Len(t, GetDefaultExtensionsToIgnore(), 38)
 	})
 }
