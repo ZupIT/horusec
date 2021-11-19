5611c8e5713dc9bb89a9e998d697bf4b2c2e73f2
matheusalcantarazup <84723211+matheusalcantarazup@users.noreply.github.com>
2021-11-19 09:00:11 -0300

    workdir:chore - improve tests and rename public functions (#788)
    This commit improve the current tests and add new ones. The improvement
    was made to have a better assertiveness and cover more scenarios.
    
    The public functions `NewWorkDir` and `ParseInterfaceToStruct` was
    renamed to `Default` and `MustParseWorkDir` respectively to follow the
    same API from toolsconfig and customimages packages to have consistency
    between them.
    
    This commit also remove the unused method `Type` from WorkDir struct.
    
    Updates #718
    
    Signed-off-by: Matheus Alcantara <matheus.alcantara@zup.com.br>

--- internal/entities/workdir/workdir_test.go
+++ internal/entities/workdir/workdir_test.go
@@ -12,60 +12,172 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package workdir
+package workdir_test
 
 import (
-	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ZupIT/horusec-devkit/pkg/enums/languages"
+	"github.com/ZupIT/horusec/internal/entities/workdir"
 )
 
+func TestDefaultReturnEmptyWorkDir(t *testing.T) {
+	wd := workdir.Default()
+
+	assert.Empty(t, wd.Go)
+	assert.Empty(t, wd.CSharp)
+	assert.Empty(t, wd.Ruby)
+	assert.Empty(t, wd.Python)
+	assert.Empty(t, wd.Java)
+	assert.Empty(t, wd.Kotlin)
+	assert.Empty(t, wd.JavaScript)
+	assert.Empty(t, wd.Leaks)
+	assert.Empty(t, wd.HCL)
+	assert.Empty(t, wd.PHP)
+	assert.Empty(t, wd.C)
+	assert.Empty(t, wd.Yaml)
+	assert.Empty(t, wd.Generic)
+	assert.Empty(t, wd.Elixir)
+	assert.Empty(t, wd.Shell)
+	assert.Empty(t, wd.Dart)
+	assert.Empty(t, wd.Nginx)
+}
+
+func TestMustParseWorkDir(t *testing.T) {
+	testcases := []struct {
+		name     string
+		input    map[string]interface{}
+		expected *workdir.WorkDir
+	}{
+		{
+			name: "Should successfully parse work dir",
+			input: map[string]interface{}{
+				"go": []string{
+					"some/random/path",
+				},
+				"csharp": []string{},
+				"dart": []string{
+					"other/random/path",
+				},
+			},
+			expected: &workdir.WorkDir{
+				Go: []string{
+					"some/random/path",
+				},
+				CSharp:     make([]string, 0),
+				Ruby:       make([]string, 0),
+				Python:     make([]string, 0),
+				Java:       make([]string, 0),
+				Kotlin:     make([]string, 0),
+				JavaScript: make([]string, 0),
+				Leaks:      make([]string, 0),
+				HCL:        make([]string, 0),
+				PHP:        make([]string, 0),
+				C:          make([]string, 0),
+				Yaml:       make([]string, 0),
+				Generic:    make([]string, 0),
+				Elixir:     make([]string, 0),
+				Shell:      make([]string, 0),
+				Nginx:      make([]string, 0),
+				Dart: []string{
+					"other/random/path",
+				},
+			},
+		},
+		{
+			name: "Should fail on parse invalid work dir and return default",
+			input: map[string]interface{}{
+				"go": "invalid type value",
+			},
+			expected: workdir.Default(),
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			wd := workdir.MustParseWorkDir(tt.input)
+
+			assert.Equal(t, tt.expected, wd)
+		})
+	}
+}
+
+func TestLanguagePaths(t *testing.T) {
+	wd := workdir.MustParseWorkDir(map[string]interface{}{
+		"go":     []string{"some/random/path"},
+		"csharp": []string{"other/path"},
+	})
+
+	expected := map[languages.Language][]string{
+		languages.Go:         {"some/random/path"},
+		languages.CSharp:     {"other/path"},
+		languages.Ruby:       make([]string, 0),
+		languages.Python:     make([]string, 0),
+		languages.Java:       make([]string, 0),
+		languages.Kotlin:     make([]string, 0),
+		languages.Javascript: make([]string, 0),
+		languages.Leaks:      make([]string, 0),
+		languages.HCL:        make([]string, 0),
+		languages.Generic:    make([]string, 0),
+		languages.PHP:        make([]string, 0),
+		languages.C:          make([]string, 0),
+		languages.Yaml:       make([]string, 0),
+		languages.Elixir:     make([]string, 0),
+		languages.Shell:      make([]string, 0),
+		languages.Dart:       make([]string, 0),
+		languages.Nginx:      make([]string, 0),
+	}
+
+	assert.Equal(t, expected, wd.LanguagePaths())
+}
+
+func TestPathsOfLanguage(t *testing.T) {
+	goPaths := []string{"some/random/path"}
+	wd := workdir.MustParseWorkDir(map[string]interface{}{
+		"go": goPaths,
+	})
+
+	assert.Equal(
+		t,
+		goPaths,
+		wd.PathsOfLanguage(languages.Go),
+		"Expected equal paths to a language that have a custom work dir",
+	)
+
+	assert.Equal(
+		t,
+		[]string{""},
+		wd.PathsOfLanguage(languages.Java),
+		"Expected a slice with an empty path for a language that does not have custom work dir",
+	)
+}
+
 func TestString(t *testing.T) {
-	t.Run("should success parse workdir to string", func(t *testing.T) {
-		workDir := &WorkDir{}
-		assert.NotEmpty(t, workDir.String())
-	})
+	wd := workdir.Default()
+
+	expected := `
+{
+  "go": [],
+  "csharp": [],
+  "ruby": [],
+  "python": [],
+  "java": [],
+  "kotlin": [],
+  "javaScript": [],
+  "leaks": [],
+  "hcl": [],
+  "php": [],
+  "c": [],
+  "yaml": [],
+  "generic": [],
+  "elixir": [],
+  "shell": [],
+  "dart": [],
+  "nginx": []
 }
+	`
 
-func TestSet(t *testing.T) {
-	t.Run("should error parse interface to workdir", func(t *testing.T) {
-		workDir := &WorkDir{}
-
-		assert.NotPanics(t, func() {
-			workDir.ParseInterfaceToStruct(math.NaN())
-		})
-	})
-	t.Run("should success parse interface to workdir", func(t *testing.T) {
-		workDir := &WorkDir{}
-
-		assert.NotPanics(t, func() {
-			workDir.ParseInterfaceToStruct(workDir)
-		})
-	})
-}
-
-func TestType(t *testing.T) {
-	t.Run("should return type", func(t *testing.T) {
-		workDir := &WorkDir{}
-		assert.Empty(t, workDir.Type())
-	})
-}
-
-func TestMap(t *testing.T) {
-	t.Run("should success return a new map of workdir", func(t *testing.T) {
-		workDir := &WorkDir{}
-		assert.NotEmpty(t, workDir.LanguagePaths())
-	})
-}
-
-func TestGetArrayByLanguage(t *testing.T) {
-	t.Run("should success return a GetArrayByLanguage", func(t *testing.T) {
-		workDir := &WorkDir{
-			JavaScript: []string{"frontend"},
-		}
-		assert.NotEmpty(t, workDir.PathsOfLanguage(languages.Javascript))
-	})
+	assert.JSONEq(t, expected, wd.String())
 }
