c589c299e550e1d2c249d5c8f3e6b3d9fcfe75f1
Matheus Alcantara <matheus.alcantara@zup.com.br>
2021-11-26 13:40:15 -0300

    images:chore - upgrade Docker tags
    This commit upgrade the tags for almost all languages that is supported
    by Horusec.
    
    Python was not upgraded because the safety and bandit was not released
    with official support for Python 3.10.
    
    Generic and HCL was not upgraded too because we need to upgrade the
    installation script of tools that is installed on Dockerfile (tfsec,
    semgrep and dependency check) which will be done separately.
    
    Signed-off-by: Matheus Alcantara <matheus.alcantara@zup.com.br>

--- internal/enums/images/images.go
+++ internal/enums/images/images.go
@@ -18,18 +18,18 @@ import "github.com/ZupIT/horusec-devkit/pkg/enums/languages"
 
 const (
 	DefaultRegistry = "docker.io"
-	C               = "horuszup/horusec-c:v1.0.0"
-	Csharp          = "horuszup/horusec-csharp:v1.0.1"
-	Elixir          = "horuszup/horusec-elixir:v1.0.0"
+	C               = "horuszup/horusec-c:v1.0.1"
+	Csharp          = "horuszup/horusec-csharp:v1.1.0"
+	Elixir          = "horuszup/horusec-elixir:v1.1.0"
 	Generic         = "horuszup/horusec-generic:v1.1.0"
-	Go              = "horuszup/horusec-go:v1.0.1"
+	Go              = "horuszup/horusec-go:v1.2.0"
 	HCL             = "horuszup/horusec-hcl:v1.1.0"
-	Javascript      = "horuszup/horusec-js:v1.0.0"
-	Leaks           = "horuszup/horusec-leaks:v1.0.1"
-	PHP             = "horuszup/horusec-php:v1.0.0"
+	Javascript      = "horuszup/horusec-js:v1.2.0"
+	Leaks           = "horuszup/horusec-leaks:v1.1.0"
+	PHP             = "horuszup/horusec-php:v1.0.1"
 	Python          = "horuszup/horusec-python:v1.0.0"
-	Ruby            = "horuszup/horusec-ruby:v1.0.2"
-	Shell           = "horuszup/horusec-shell:v1.0.0"
+	Ruby            = "horuszup/horusec-ruby:v1.1.0"
+	Shell           = "horuszup/horusec-shell:v1.0.1"
 )
 
 func MapValues() map[languages.Language]string {
