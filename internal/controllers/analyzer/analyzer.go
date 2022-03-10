ZupIT/horusec

commit cd839ef46cdb28979ec1251ab3589307ab146b6a

errors:chore - move errors from enums/error pkg to respective pkgs (#862)
Move errors declarations on enums/errors package to their packages that
use these errors. Since all errors was moved, the enums/error package
was also removed.

Signed-off-by: Matheus Alcantara <matheus.alcantara@zup.com.br>

internal/services/formatters/ruby/brakeman/formatter.go
@@ -16,6 +16,7 @@ package brakeman
 
 import (
 	"encoding/json"
+	"errors"
 	"strings"
 
 	"github.com/ZupIT/horusec-devkit/pkg/entities/vulnerability"
@@ -24,13 +25,14 @@ import (
 	"github.com/ZupIT/horusec-devkit/pkg/utils/logger"
 
 	dockerEntities "github.com/ZupIT/horusec/internal/entities/docker"
-	errorsEnums "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/enums/images"
 	"github.com/ZupIT/horusec/internal/helpers/messages"
 	"github.com/ZupIT/horusec/internal/services/formatters"
 	"github.com/ZupIT/horusec/internal/services/formatters/ruby/brakeman/entities"
 	vulnhash "github.com/ZupIT/horusec/internal/utils/vuln_hash"
 )
+
+var ErrNotFoundRailsProject = errors.New("brakeman only works on Ruby On Rails project")
 
 type Formatter struct {
 	formatters.IService
@@ -84,7 +86,7 @@ func (f *Formatter) parseOutput(containerOutput, projectSubPath string) error {
 
 func (f *Formatter) newContainerOutputFromString(containerOutput string) (output entities.Output, err error) {
 	if f.isNotFoundRailsProject(containerOutput) {
-		return entities.Output{}, errorsEnums.ErrNotFoundRailsProject
+		return entities.Output{}, ErrNotFoundRailsProject
 	}
 
 	err = json.Unmarshal([]byte(containerOutput), &output)

internal/services/formatters/ruby/bundler/formatter.go
@@ -16,6 +16,7 @@ package bundler
 
 import (
 	"bufio"
+	"errors"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -28,7 +29,6 @@ import (
 	"github.com/ZupIT/horusec-devkit/pkg/utils/logger"
 
 	dockerEntities "github.com/ZupIT/horusec/internal/entities/docker"
-	errorsEnums "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/enums/images"
 	"github.com/ZupIT/horusec/internal/helpers/messages"
 	"github.com/ZupIT/horusec/internal/services/formatters"
@@ -36,6 +36,11 @@ import (
 	"github.com/ZupIT/horusec/internal/utils/file"
 	vulnhash "github.com/ZupIT/horusec/internal/utils/vuln_hash"
 )
+
+// ErrGemLockNotFound occurs when bundles does not find gemfile.lock.
+//
+// nolint: lll
+var ErrGemLockNotFound = errors.New("project doesn't have a gemfile.lock file, it would be a good idea to commit it so horusec can check for vulnerabilities")
 
 type Formatter struct {
 	formatters.IService
@@ -85,7 +90,7 @@ func (f *Formatter) getDockerConfig(projectSubPath string) *dockerEntities.Analy
 
 func (f *Formatter) verifyGemLockError(output string) error {
 	if strings.Contains(output, "No such file or directory") && strings.Contains(output, "Errno::ENOENT") {
-		return errorsEnums.ErrGemLockNotFound
+		return ErrGemLockNotFound
 	}
 
 	return nil

internal/controllers/requirements/docker/docker.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/ZupIT/horusec-devkit/pkg/utils/logger"
 
-	errorsEnums "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/helpers/messages"
 )
 
@@ -32,7 +31,13 @@ const (
 	MinSubVersionDockerAccept = 0o3
 )
 
-var ErrMinVersion = fmt.Errorf("%v.%v", MinVersionDockerAccept, MinSubVersionDockerAccept)
+var (
+	// ErrMinVersion occur when the installed Docker version is not the minimum supported.
+	ErrMinVersion = fmt.Errorf("%v.%v", MinVersionDockerAccept, MinSubVersionDockerAccept)
+
+	// ErrDockerNotInstalled occurs when Docker is not installed.
+	ErrDockerNotInstalled = errors.New("docker not found. Please check and try again")
+)
 
 type RequirementDocker struct{}
 
@@ -55,7 +60,7 @@ func (r *RequirementDocker) validateIfDockerIsInstalled() (string, error) {
 		return "", err
 	}
 	if !r.checkIfContainsDockerVersion(response) {
-		return "", errorsEnums.ErrDockerNotInstalled
+		return "", ErrDockerNotInstalled
 	}
 	return response, r.checkIfDockerIsRunning()
 }
@@ -106,7 +111,7 @@ func (r *RequirementDocker) validateIfDockerIsRunningInMinVersion(response strin
 func (r *RequirementDocker) extractDockerVersionFromString(response string) (int, int, error) {
 	responseSpited := strings.Split(strings.ToLower(response), "docker version ")
 	if len(responseSpited) < 1 || len(responseSpited) > 1 && len(responseSpited[1]) < 8 {
-		return 0, 0, errorsEnums.ErrDockerNotInstalled
+		return 0, 0, ErrDockerNotInstalled
 	}
 	return r.getVersionAndSubVersion(responseSpited[1])
 }
@@ -118,11 +123,11 @@ func (r *RequirementDocker) checkIfContainsDockerVersion(response string) bool {
 func (r *RequirementDocker) getVersionAndSubVersion(fullVersion string) (int, int, error) {
 	version, err := strconv.Atoi(fullVersion[0:2])
 	if err != nil {
-		return 0, 0, errorsEnums.ErrDockerNotInstalled
+		return 0, 0, ErrDockerNotInstalled
 	}
 	subversion, err := strconv.Atoi(fullVersion[3:5])
 	if err != nil {
-		return 0, 0, errorsEnums.ErrDockerNotInstalled
+		return 0, 0, ErrDockerNotInstalled
 	}
 	return version, subversion, nil
 }

internal/controllers/requirements/git/git.go
@@ -15,6 +15,7 @@
 package git
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
 	"strconv"
@@ -22,7 +23,6 @@ import (
 
 	"github.com/ZupIT/horusec-devkit/pkg/utils/logger"
 
-	errorsEnums "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/helpers/messages"
 )
 
@@ -31,7 +31,16 @@ const (
 	MinSubVersionGitAccept = 0o1
 )
 
-var ErrMinVersion = fmt.Errorf("%v.%v", MinVersionGitAccept, MinSubVersionGitAccept)
+var (
+	// ErrMinVersion is the error logged when the installed Git version is not the minimum supported.
+	ErrMinVersion = fmt.Errorf("%v.%v", MinVersionGitAccept, MinSubVersionGitAccept)
+
+	// ErrGitNotInstalled occurs when Git is not installed.
+	ErrGitNotInstalled = errors.New("git not found. Please check and try again")
+
+	// ErrGitLowerVersion occur when the installed Git version is not the minimum supported.
+	ErrGitLowerVersion = errors.New("git version is lower of 2.01. Please check and try again")
+)
 
 type RequirementGit struct{}
 
@@ -53,7 +62,7 @@ func (r *RequirementGit) validateIfGitIsInstalled() (string, error) {
 		return "", err
 	}
 	if !r.checkIfContainsGitVersion(response) {
-		return "", errorsEnums.ErrGitNotInstalled
+		return "", ErrGitNotInstalled
 	}
 	return response, nil
 }
@@ -83,10 +92,10 @@ func (r *RequirementGit) validateIfGitIsRunningInMinVersion(response string) err
 	}
 	if version < MinVersionGitAccept {
 		logger.LogErrorWithLevel(messages.MsgErrorWhenGitIsLowerVersion, ErrMinVersion)
-		return errorsEnums.ErrGitLowerVersion
+		return ErrGitLowerVersion
 	} else if version == MinVersionGitAccept && subversion < MinSubVersionGitAccept {
 		logger.LogErrorWithLevel(messages.MsgErrorWhenGitIsLowerVersion, ErrMinVersion)
-		return errorsEnums.ErrGitLowerVersion
+		return ErrGitLowerVersion
 	}
 	return nil
 }
@@ -94,7 +103,7 @@ func (r *RequirementGit) validateIfGitIsRunningInMinVersion(response string) err
 func (r *RequirementGit) extractGitVersionFromString(response string) (int, int, error) {
 	responseSpited := strings.Split(strings.ToLower(response), "git version ")
 	if len(responseSpited) < 1 || len(responseSpited) > 1 && len(responseSpited[1]) < 3 {
-		return 0, 0, errorsEnums.ErrGitNotInstalled
+		return 0, 0, ErrGitNotInstalled
 	}
 	return r.getVersionAndSubVersion(responseSpited[1])
 }
@@ -106,11 +115,11 @@ func (r *RequirementGit) checkIfContainsGitVersion(response string) bool {
 func (r *RequirementGit) getVersionAndSubVersion(fullVersion string) (int, int, error) {
 	version, err := strconv.Atoi(fullVersion[0:1])
 	if err != nil {
-		return 0, 0, errorsEnums.ErrGitNotInstalled
+		return 0, 0, ErrGitNotInstalled
 	}
 	subversion, err := strconv.Atoi(fullVersion[2:4])
 	if err != nil {
-		return 0, 0, errorsEnums.ErrGitNotInstalled
+		return 0, 0, ErrGitNotInstalled
 	}
 	return version, subversion, nil
 }

internal/enums/errors/errors.go
@@ -1,41 +1,0 @@
-// Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package errors
-
-import "errors"
-
-// Occurs when o git is not installed in O.S
-
-var ErrGitNotInstalled = errors.New("{HORUSEC_CLI} Error Git not found. Please check and try again")
-
-// Occurs when o git is lower version required
-
-var ErrGitLowerVersion = errors.New("{HORUSEC_CLI} Error Git version is lower of 2.01. Please check and try again")
-
-// Occurs when o docker is not installed in O.S
-
-var ErrDockerNotInstalled = errors.New("{HORUSEC_CLI} Error Docker not found. Please check and try again")
-
-// Occurs when not found rails project
-
-var ErrNotFoundRailsProject = errors.New("brakeman only works on Ruby On Rails project")
-
-// Occurs when gem lock not found
-
-var ErrGemLockNotFound = errors.New(
-	"{HORUSEC_CLI} Error It looks like your project doesn't have a gemfile.lock file," +
-		" it would be a good idea to commit it so horusec can check for vulnerabilities")
-
-var ErrImageTagCmdRequired = errors.New("{ERROR_DOCKER_API} required exists Image, Tag and CMD not empty")

internal/services/docker/docker_api.go
@@ -39,10 +39,12 @@ import (
 
 	"github.com/ZupIT/horusec/config"
 	"github.com/ZupIT/horusec/internal/entities/docker"
-	enumErrors "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/enums/images"
 	"github.com/ZupIT/horusec/internal/helpers/messages"
 )
+
+// ErrImageTagCmdRequired occurs when an docker image or docker command is empty to start analysis.
+var ErrImageTagCmdRequired = errors.New("image or cmd is empty")
 
 // Docker is the interface that abstract the Docker API.
 type Docker interface {
@@ -122,7 +124,7 @@ func New(client Client, cfg *config.Config, analysisID uuid.UUID) *API {
 
 func (d *API) CreateLanguageAnalysisContainer(data *docker.AnalysisData) (containerOutPut string, err error) {
 	if data.IsInvalid() {
-		return "", enumErrors.ErrImageTagCmdRequired
+		return "", ErrImageTagCmdRequired
 	}
 
 	return d.logStatusAndExecuteCRDContainer(data.GetCustomOrDefaultImage(), d.replaceCMDAnalysisID(data.CMD))

internal/services/docker/docker_api_test.go
@@ -30,7 +30,6 @@ import (
 
 	cliConfig "github.com/ZupIT/horusec/config"
 	dockerEntities "github.com/ZupIT/horusec/internal/entities/docker"
-	errorsenum "github.com/ZupIT/horusec/internal/enums/errors"
 	"github.com/ZupIT/horusec/internal/services/docker/client"
 	"github.com/ZupIT/horusec/internal/utils/testutil"
 )
@@ -72,7 +71,7 @@ func TestDockerAPI_CreateLanguageAnalysisContainer(t *testing.T) {
 		})
 
 		assert.Error(t, err)
-		assert.ErrorIs(t, err, errorsenum.ErrImageTagCmdRequired)
+		assert.ErrorIs(t, err, ErrImageTagCmdRequired)
 	})
 
 	t.Run("Should return error when cmd is empty", func(t *testing.T) {
@@ -83,7 +82,7 @@ func TestDockerAPI_CreateLanguageAnalysisContainer(t *testing.T) {
 		})
 
 		assert.Error(t, err)
-		assert.ErrorIs(t, err, errorsenum.ErrImageTagCmdRequired)
+		assert.ErrorIs(t, err, ErrImageTagCmdRequired)
 	})
 
 	t.Run("Should return error when pull image aleatory", func(t *testing.T) {

commit b6a1cf5eb71cb887cf186bbe7f2f7ec57ca3f14b

formatters/ruby:chore - removing unnecessary error messages
Previously if the project had ruby between programming languages but is
not a ruby on rails project or does not have a Gemfile.lock an error
would be reported for each scenario.

This pull request changes these error messages introduced in commits
cd839ef and 9245d7d to be considered just warnings, as there is no
need to point them out as errors, since it is only intended to
inform the user.

Signed-off-by: Nathan Martins <nathan.martins@zup.com.br>

internal/controllers/analyzer/analyzer.go
@@ -448,9 +448,12 @@ func (a *Analyzer) removeWarningsFromErrors() {
 }
 
 // isWarning workaround to check if the message it's form a warning until the formatters are refactored
+// nolint:gocyclo // necessary complexity, but will be removed in the future
 func (a *Analyzer) isWarning(err string) bool {
 	return strings.Contains(err, messages.MsgErrorPackageLockJSONNotFound) ||
 		strings.Contains(err, messages.MsgErrorYarnLockNotFound) ||
 		strings.Contains(err, messages.MsgErrorNotFoundRequirementsTxt) ||
-		strings.Contains(err, messages.MsgWarnPathIsInvalidGitRepository)
+		strings.Contains(err, messages.MsgWarnPathIsInvalidGitRepository) ||
+		strings.Contains(err, messages.MsgWarnBrakemanNotRubyOnRailsProject) ||
+		strings.Contains(err, messages.MsgWarnGemfileIsRequiredForBundler)
 }

internal/helpers/messages/warn.go
@@ -55,5 +55,7 @@ const (
 	// TODO: Remove MsgWarnAnalysisContainsOutdatedHash before release v2.10.0
 	MsgWarnAnalysisContainsOutdatedHash = "{HORUSEC_CLI} YOUR CONFIGURATION FILE CONTAINS SOME HASHES THAT WILL NO " +
 		"LONGER BE VALID AS OF v2.10.0 IS RELEASED. PLEASE UPDATE YOUR CONFIGURATION FILE WITH THE FOLLOWING HASHES:"
-	MsgWarnPathIsInvalidGitRepository = "{HORUSEC_CLI} The current path it's not a valid git repository"
+	MsgWarnPathIsInvalidGitRepository    = "{HORUSEC_CLI} The current path it's not a valid git repository"
+	MsgWarnBrakemanNotRubyOnRailsProject = "brakeman only works on Ruby On Rails project"
+	MsgWarnGemfileIsRequiredForBundler   = "Gemfile.lock file is required to execute Bundler analysis"
 )

internal/services/formatters/ruby/brakeman/formatter.go
@@ -42,7 +42,7 @@ const (
 	notFoundError = "please supply the path to a rails application"
 )
 
-var ErrNotFoundRailsProject = errors.New("brakeman only works on Ruby On Rails project")
+var ErrNotFoundRailsProject = errors.New(messages.MsgWarnBrakemanNotRubyOnRailsProject)
 
 type Formatter struct {
 	formatters.IService

internal/services/formatters/ruby/bundler/formatter.go
@@ -41,7 +41,7 @@ import (
 // nolint: stylecheck
 // We actually want that this error message be capitalized since the file name that was
 // not found is capitalized.
-var ErrGemLockNotFound = errors.New("Gemfile.lock file is required to execute Bundler analysis")
+var ErrGemLockNotFound = errors.New(messages.MsgWarnGemfileIsRequiredForBundler)
 
 type Formatter struct {
 	formatters.IService