630a87054d642018160b14cbcf2cb119b669507f
Wilian Gabriel <wilian.silva@zup.com.br>
2022-03-23 10:14:02 -0300

    javascript::chore - Adding improvements on javascript rules (#1043)
    In this commit I changed to add Safe and Unsafe code
    in all rules of the javascript. Reference from issue #630 and #1020.
    
    Signed-off-by: Wilian Gabriel <wilian.silva@zup.com.br>

--- internal/services/engines/javascript/rules_test.go
+++ internal/services/engines/javascript/rules_test.go
@@ -15,7 +15,6 @@
 package javascript
 
 import (
-	"fmt"
 	"path/filepath"
 	"testing"
 
@@ -31,12 +30,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-1",
 			Rule:     NewNoLogSensitiveInformationInConsole(),
 			Src:      SampleVulnerableHSJAVASCRIPT1,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-1", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-1.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `console.log("user email: ", email)`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-1", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-1.test"),
 						Line:     2,
 						Column:   0,
 					},
@@ -44,7 +43,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `console.debug("user password: ", pwd)`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-1", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-1.test"),
 						Line:     3,
 						Column:   0,
 					},
@@ -55,12 +54,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-2",
 			Rule:     NewNoUseEval(),
 			Src:      SampleVulnerableHSJAVASCRIPT2,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-2", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-2.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `eval(foo);`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-2", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-2.test"),
 						Line:     3,
 						Column:   1,
 					},
@@ -71,12 +70,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-3",
 			Rule:     NewNoDisableTlsRejectUnauthorized(),
 			Src:      SampleVulnerableHSJAVASCRIPT3,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-3", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-3.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-3", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-3.test"),
 						Line:     2,
 						Column:   12,
 					},
@@ -87,12 +86,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-4",
 			Rule:     NewNoUseMD5Hashing(),
 			Src:      SampleVulnerableHSJAVASCRIPT4,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-4", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-4.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `const hash = crypto.createHash('md5')`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-4", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-4.test"),
 						Line:     2,
 						Column:   20,
 					},
@@ -103,12 +102,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-5",
 			Rule:     NewNoUseSHA1Hashing(),
 			Src:      SampleVulnerableHSJAVASCRIPT5,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-5", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-5.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `const hash = crypto.createHash('sha1')`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-5", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-5.test"),
 						Line:     2,
 						Column:   20,
 					},
@@ -119,12 +118,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-6",
 			Rule:     NewNoUseWeakRandom(),
 			Src:      SampleVulnerableHSJAVASCRIPT6,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-6", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-6.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `return Math.random();`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-6", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-6.test"),
 						Line:     3,
 						Column:   8,
 					},
@@ -135,12 +134,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-7",
 			Rule:     NewNoReadFileUsingDataFromRequest(),
 			Src:      SampleVulnerableHSJAVASCRIPT7,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-7", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-7.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `return fs.readFileSync(req.body, 'utf8')`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-7", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-7.test"),
 						Line:     3,
 						Column:   10,
 					},
@@ -151,12 +150,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-8",
 			Rule:     NewNoCreateReadStreamUsingDataFromRequest(),
 			Src:      SampleVulnerableHSJAVASCRIPT8,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-8", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-8.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `return fs.createReadStream(req.body)`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-8", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-8.test"),
 						Line:     3,
 						Column:   10,
 					},
@@ -167,12 +166,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-9",
 			Rule:     NewSQLInjectionUsingParams(),
 			Src:      SampleVulnerableHSJAVASCRIPT9,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-9", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-9.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `Model.find({ where: { foo: req.body}});`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-9", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-9.test"),
 						Line:     3,
 						Column:   6,
 					},
@@ -183,12 +182,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-10",
 			Rule:     NewXMLParsersShouldNotBeVulnerableToXXEAttacks(),
 			Src:      SampleVulnerableHSJAVASCRIPT10,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-10", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-10.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `var xmlDoc = libxml.parseXmlString(xml, {});`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-10", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-10.test"),
 						Line:     4,
 						Column:   19,
 					},
@@ -199,12 +198,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-11",
 			Rule:     NewOriginsNotVerified(),
 			Src:      SampleVulnerableHSJAVASCRIPT11,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-11", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-11.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `popup.postMessage("message", "https://foo.bar", "*");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-11", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-11.test"),
 						Line:     4,
 						Column:   6,
 					},
@@ -212,7 +211,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `window.addEventListener("message", (event) => {});`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-11", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-11.test"),
 						Line:     8,
 						Column:   7,
 					},
@@ -223,12 +222,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-12",
 			Rule:     NewWeakSSLTLSProtocolsShouldNotBeUsed(),
 			Src:      SampleVulnerableHSJAVASCRIPT12,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-12", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-12.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `secureProtocol: 'TLSv1_method'`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-12", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-12.test"),
 						Line:     4,
 						Column:   19,
 					},
@@ -236,7 +235,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `secureProtocol: 'TLSv1.1'`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-12", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-12.test"),
 						Line:     10,
 						Column:   19,
 					},
@@ -247,12 +246,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-13",
 			Rule:     NewWebSQLDatabasesShouldNotBeUsed(),
 			Src:      SampleVulnerableHSJAVASCRIPT13,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-13", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-13.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `const db = window.openDatabase();`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-13", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-13.test"),
 						Line:     2,
 						Column:   11,
 					},
@@ -263,12 +262,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-14",
 			Rule:     NewLocalStorageShouldNotBeUsed(),
 			Src:      SampleVulnerableHSJAVASCRIPT14,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-14", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-14.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `localStorage.setItem("foo", "bar");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-14", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-14.test"),
 						Line:     3,
 						Column:   1,
 					},
@@ -276,7 +275,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `sessionStorage.setItem("foo", "bar");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-14", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-14.test"),
 						Line:     7,
 						Column:   1,
 					},
@@ -287,14 +286,14 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-15",
 			Rule:     NewDebuggerStatementsShouldNotBeUsed(),
 			Src:      SampleVulnerableHSJAVASCRIPT15,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-15", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-15.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `debugger;`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-15", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-15.test"),
 						Line:     2,
-						Column:   1,
+						Column:   0,
 					},
 				},
 			},
@@ -303,12 +302,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-16",
 			Rule:     NewAlertStatementsShouldNotBeUsed(),
 			Src:      SampleVulnerableHSJAVASCRIPT16,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-16", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-16.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `alert("testing");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-16", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-16.test"),
 						Line:     3,
 						Column:   1,
 					},
@@ -316,7 +315,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `confirm("testing");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-16", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-16.test"),
 						Line:     7,
 						Column:   1,
 					},
@@ -324,7 +323,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `prompt("testing");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-16", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-16.test"),
 						Line:     11,
 						Column:   1,
 					},
@@ -335,12 +334,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-17",
 			Rule:     NewStaticallyServingHiddenFilesIsSecuritySensitive(),
 			Src:      SampleVulnerableHSJAVASCRIPT17,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-17", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-17.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `dotfiles : 'allow'`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-17", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-17.test"),
 						Line:     3,
 						Column:   2,
 					},
@@ -351,12 +350,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-18",
 			Rule:     NewUsingIntrusivePermissionsWithGeolocation(),
 			Src:      SampleVulnerableHSJAVASCRIPT18,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-18", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-18.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `navigator.geolocation.getCurrentPosition(success, error, {});`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-18", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-18.test"),
 						Line:     10,
 						Column:   10,
 					},
@@ -367,12 +366,12 @@ func TestRulesVulnerableCode(t *testing.T) {
 			Name:     "HS-JAVASCRIPT-19",
 			Rule:     NewHavingAPermissiveCrossOriginResourceSharingPolicy(),
 			Src:      SampleVulnerableHSJAVASCRIPT19,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-19", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-19.test"),
 			Findings: []engine.Finding{
 				{
 					CodeSample: `res.header("Access-Control-Allow-Origin", "*");`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-19", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-19.test"),
 						Line:     7,
 						Column:   14,
 					},
@@ -380,7 +379,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `app.get('/products/:id', cors(), function (req, res, next) {`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-19", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-19.test"),
 						Line:     6,
 						Column:   25,
 					},
@@ -388,7 +387,7 @@ func TestRulesVulnerableCode(t *testing.T) {
 				{
 					CodeSample: `origin: '*',`,
 					SourceLocation: engine.Location{
-						Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-19", ".test")),
+						Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-19.test"),
 						Line:     3,
 						Column:   2,
 					},
@@ -404,46 +403,58 @@ func TestRulesSafeCode(t *testing.T) {
 	tempDir := t.TempDir()
 	testcases := []*testutil.RuleTestCase{
 		{
+			Name:     "HS-JAVASCRIPT-1",
+			Rule:     NewNoLogSensitiveInformationInConsole(),
+			Src:      SampleSafeHSJAVASCRIPT1,
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-1.test"),
+		},
+		{
 			Name:     "HS-JAVASCRIPT-2",
 			Rule:     NewNoUseEval(),
 			Src:      SampleSafeHSJAVASCRIPT2,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-1", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-2.test"),
+		},
+		{
+			Name:     "HS-JAVASCRIPT-3",
+			Rule:     NewNoDisableTlsRejectUnauthorized(),
+			Src:      SampleSafeHSJAVASCRIPT3,
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-3.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-9",
 			Rule:     NewSQLInjectionUsingParams(),
 			Src:      SampleSafeHSJAVASCRIPT9,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-9", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-9.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-10",
 			Rule:     NewXMLParsersShouldNotBeVulnerableToXXEAttacks(),
 			Src:      SampleSafeHSJAVASCRIPT10,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-10", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-10.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-11",
 			Rule:     NewOriginsNotVerified(),
 			Src:      SampleSafeHSJAVASCRIPT11,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-11", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-11.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-12",
 			Rule:     NewWeakSSLTLSProtocolsShouldNotBeUsed(),
 			Src:      SampleSafeHSJAVASCRIPT12,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-12", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-12.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-17",
 			Rule:     NewStaticallyServingHiddenFilesIsSecuritySensitive(),
 			Src:      SampleSafeHSJAVASCRIPT17,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-17", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-17.test"),
 		},
 		{
 			Name:     "HS-JAVASCRIPT-19",
 			Rule:     NewHavingAPermissiveCrossOriginResourceSharingPolicy(),
 			Src:      SampleSafeHSJAVASCRIPT19,
-			Filename: filepath.Join(tempDir, fmt.Sprintf("%s%s", "HS-JAVASCRIPT-19", ".test")),
+			Filename: filepath.Join(tempDir, "HS-JAVASCRIPT-19.test"),
 		},
 	}
 
