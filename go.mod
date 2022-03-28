87aed338ba633e91fd85e2cb81f05517c87d6891
Nathan Martins <nathan.martins@zup.com.br>
2022-03-28 14:08:14 -0300

    workflow:feature - updating release workflows to semantic analysis
    Previously it was not possible to build the binary for mac, as we did
    not have a C compiler with a valid license in the main workflow. This
    pull request adds a new workflow that is activated after completion of
    the final-release, beta-release, or rc-release. This new workflow will
    build the binaries directly on a mac machine and update the release with
    the new binaries
    
    Signed-off-by: Nathan Martins <nathan.martins@zup.com.br>

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/go-enry/go-enry/v2 v2.8.0
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/gocarina/gocsv v0.0.0-20220304222734-caabc5f00d30
+	github.com/google/go-github/v40 v40.0.0
 	github.com/google/uuid v1.3.0
 	github.com/iancoleman/strcase v0.2.0
 	github.com/magefile/mage v1.12.1
@@ -23,6 +24,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 )
 
 require (
@@ -39,7 +41,6 @@ require (
 	github.com/go-enry/go-oniguruma v1.2.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-github/v40 v40.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -67,6 +68,7 @@ require (
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220114231437-d2e6a121cae0 // indirect
 	google.golang.org/grpc v1.44.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- magefile.go
+++ magefile.go
@@ -14,15 +14,28 @@
 
 //go:build mage
 
-// This is horusec private mage functions, check https://github.com/ZupIT/horusec-devkit/tree/main/pkg/utils/mageutils for basics functions.
+// This is horusec private mage functions, check https://github.com/ZupIT/horusec-devkit/tree/main/pkg/utils/mageutils
+// for basics functions.
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	// mage:import
 	_ "github.com/ZupIT/horusec-devkit/pkg/utils/mageutils"
+	"github.com/google/go-github/v40/github"
 	"github.com/magefile/mage/sh"
+	"golang.org/x/oauth2"
+)
+
+// env vars
+const (
+	envRepositoryOrg  = "HORUSEC_REPOSITORY_ORG"
+	envRepositoryName = "HORUSEC_REPOSITORY_NAME"
+	envGithubToken    = "GITHUB_TOKEN"
 )
 
 // GetCurrentDate execute "echo", `::set-output name=date::$(date "+%a %b %d %H:%M:%S %Y")`
@@ -33,4 +46,56 @@ func GetCurrentDate() error {
 	}
 
 	return sh.RunV("echo", fmt.Sprintf("::set-output name=date::%s", date))
+}
+
+// GetReleaseInfo get the latest launched release tag, including beta, rc and latest. Then print the outputs
+// 'lastLaunchedRelease' which is the found tag, and 'actualReleaseBranchName' containing the release branch name
+// related to the tag.
+func GetReleaseInfo() error {
+	tags, resp, err := newGithubClient().Repositories.ListTags(
+		context.Background(),
+		os.Getenv(envRepositoryOrg),
+		os.Getenv(envRepositoryName),
+		&github.ListOptions{Page: 1, PerPage: 1},
+	)
+
+	if github.CheckResponse(resp.Response) != nil {
+		return err
+	}
+
+	printOutputs(*tags[0].Name)
+
+	return nil
+}
+
+// newGithubClient creates a new github.Client with the GitHub auth token
+func newGithubClient() *github.Client {
+	token := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv(envGithubToken)},
+	)
+
+	return github.NewClient(oauth2.NewClient(context.Background(), token))
+}
+
+// printOutputs print the outputs 'lastLaunchedRelease' which is the found tag and 'actualReleaseBranchName'
+// containing the release branch name related to the tag.
+func printOutputs(version string) {
+	fmt.Printf("::set-output name=lastLaunchedRelease::%s\n", version)
+	fmt.Printf("::set-output name=actualReleaseBranchName::%s\n", getReleaseBranch(version))
+}
+
+// getReleaseBranch splits the version raw string into major and minor version. With this info the release branch
+// name is mounted.
+func getReleaseBranch(version string) string {
+	splittedVersion := strings.Split(removePrefixes(version), ".")
+	major := splittedVersion[0]
+	minor := splittedVersion[1]
+
+	return fmt.Sprintf("release/v%s.%s", major, minor)
+}
+
+// removePrefixes remove all the prefixes from the version, including '-rc', '-beta', 'v'.
+func removePrefixes(version string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(version, "-rc", ""),
+		"-beta", ""), "v", "")
 }
